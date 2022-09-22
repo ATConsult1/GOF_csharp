a9b52e3ac37ca4fee04db6c977be4ca6b687f63b
Andrei <a0z9@rambler.ru>
2022-09-22 11:40:46 +0300

    All 3 patterns

--- Gof/day1/Program.cs
+++ Gof/day1/Program.cs
@@ -1,23 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
 using learning2022.gof.af;
-
-
-
+using learning2022.gof.ston;
+using af = learning2022.gof.af;
+using st = learning2022.gof.ston;
 
 
 namespace learning2022.gof
 {
     class GofTest {
 
-        //public static void Main(String[] args)
-        //{
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, GOF!");
+            testFabricMethod();
 
-        //    Console.WriteLine("Hello, GOF!");
-        //    testFabricMethod();
+            testAbstractFabric(new ConcreteFactory1());
+            testAbstractFabric(new ConcreteFactory2());
 
-        //}
+            testSingleton();
+        }
+
+        static void testSingleton()
+        {
+            Singleton s1 = Singleton.getInstance();
+            Singleton s2 = Singleton.getInstance();
+
+            Console.WriteLine(s1.GetHashCode());
+            Console.WriteLine(s2.GetHashCode());
+        }
+
+        private static af.ProductA productA;
+        private static af.ProductB productB;
+
+        static void testAbstractFabric(af.AbstractProductFactory factory)
+        {
+            productA = factory.makeProductA();
+            productB = factory.makeProductB();
+        }
+
 
         static void testFabricMethod() {
 
@@ -38,10 +59,6 @@ namespace learning2022.gof
 
                 default: break;        
             }
-
-
         }
-
-
   }
 }
