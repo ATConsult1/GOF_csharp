54e5b0a2ad60b3a3c96050598a22d605c90455e9
Andrei <a0z9@rambler.ru>
2022-09-22 15:23:59 +0300

    GOF builder

---
+++ Gof/day2/Builder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning2022.gof.builder
+{
+    public abstract class Builder
+    {
+        public abstract void buildPart1();
+        public abstract void buildPart2();
+        //...
+        public abstract  Product getResult();
+    }
+
+    public class Product {
+
+        private ArrayList parts = new ArrayList(10);
+        public ArrayList getParts() { return parts; } 
+
+        public void Attach(string part)
+        { 
+          parts.Add(part);
+        }
+    }
+
+    public class ConcreteBuilder : Builder
+    {
+        Product product;
+        public ConcreteBuilder() {
+            product = new Product();
+        }
+
+        public override void buildPart1()
+        {
+            product.Attach("part1");
+        }
+
+        public override void buildPart2()
+        {
+            product.Attach("part2");
+        }
+
+        public override Product getResult()
+        {
+            return product;
+        }
+    }
+
+    class Director
+    { 
+       Builder builder;
+       public Director(Builder builder)
+        {
+            this.builder = builder;
+        }
+
+        public void BuildAll() {
+            builder.buildPart1();
+            builder.buildPart2();
+            //....
+        
+        }
+
+
+    }
+
+}
---
+++ Gof/day2/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using static andestech.learning2022.gof.Utils;
+using System.Text;
+using learning2022.gof.builder;
+
+namespace learning2022.gof
+{
+
+    class GofTest
+    {
+            public static void Main(String[] args)
+        {
+            Console.WriteLine("GOF patterns test #1");
+
+            echo("Builder");
+            Builder builder = new ConcreteBuilder();
+            Director dir = new Director(builder);
+            dir.BuildAll();
+            Product product = builder.getResult();
+            foreach (string part in product.getParts()) WriteLine(part);
+
+        }
+    }
+}
